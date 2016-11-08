e35b37b6809201bba07257c6fa37e7e185d1fdca
Alvaro Troncoso <alvaro.mc2@gmail.com>
2016-11-08 06:55:03 -0600

    curves in presentation

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,10 +8,26 @@
  * Controller of the testSohoApp
  */
 angular.module('testSohoApp')
-  .controller('HomeCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('HomeCtrl', function() {
+
+		this.slides = {
+			solutions: [{
+				quota: 12
+			}, {
+				quota: 6
+			}, {
+				quota: 3
+			}],
+			travels: [{
+				destination: 'RIO DE JANEIRO',
+				desc: 'Mar, montaña, ríos y lagunas se unieron para crear una geografía variada y hermosa.'
+			}, {
+				destination: 'LONDRES',
+				desc: 'El resultado del crecimiento de dos ciudades, ciudad de Londres y Westmister.'
+			}, {
+				destination: 'PARIS',
+				desc: 'Situada en el centro norte de Francia, París es una de las ciudades más visitadas del mundo.'
+			}]
+		};
+
+	});
